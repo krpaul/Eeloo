bb1d8d98849b979f2441966a890e57495934e5f3
krpaul <34616839+krpaul@users.noreply.github.com>
2020-02-22 12:46:30 -0700

    Added build process for Lexer Aliases

---
+++ AliasCompiler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BuildAliases
+{
+    class ConstructLexer
+    {
+        public const string FOLDERPREFIX = "./";
+        static void Main(string[] args)
+        {
+            string grammar = File.ReadAllText(FOLDERPREFIX + "LexerAliases.gram");
+            string aliases = File.ReadAllText(FOLDERPREFIX + "Aliases.yml");
+
+            Console.WriteLine(aliases);
+
+            var deserializer = new YamlDotNet.Serialization.Deserializer();
+
+            var aliasDict = deserializer.Deserialize<Dictionary<string, List<string>>>(aliases);
+
+            Regex rx = new Regex(@"\<\<\w+\>\>");
+
+            MatchCollection matches = rx.Matches(grammar);
+
+            Console.WriteLine("{0} matches found",
+                          matches.Count);
+
+            // Replace each
+            foreach (Match match in matches)
+            {
+                string aliasID = match.Value;
+                aliasID = aliasID.Substring(2, aliasID.Length - 4);
+
+                string expanded = string.Join(" | ",
+                    from syn in aliasDict[aliasID]
+                    select string.Join(" WS ",
+                        from s in syn.Split(" ") select $"'{s}'"
+                    )
+                );
+
+                grammar = grammar.Replace(match.Value, expanded);
+            }
+
+            File.WriteAllText(FOLDERPREFIX + "LexerAliases.g4", grammar);
+        }
+    }
+}
