7d5ab9bcaf15855c245ddd389904346673a8945c
krpaul <krpaul2004@gmail.com>
2020-09-22 09:11:31 -0600

    Added SpecificError abstract class

--- Eeloo/Errors/Error.cs
+++ Eeloo/Errors/Error.cs
@@ -12,10 +12,26 @@ namespace Eeloo.Errors
 {
     class Error
     {
+        protected string err_msg; 
         public Error(ParserRuleContext error_context)
         {
-            string err_msg = $"on line {error_context} ";
+            err_msg = $"occured on line {error_context.Start.Line} column {error_context.Start.Column}";
+        }
 
+        public void PrependSpecificError(SpecificError err)
+        { err_msg = err.name + err_msg; }
+    }
+
+    abstract class SpecificError : Error
+    {
+        public string name {
+            get ; 
+            set ; 
+        }
+
+        public SpecificError(SpecificError err, ParserRuleContext error_context) : base(error_context)
+        {
+            this.PrependSpecificError(this);
         }
     }
 }
