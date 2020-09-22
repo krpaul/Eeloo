957c059df32e3ffe6c68df6329f2b40a45b6af1d
Kevin Paul <krpaul2004@gmail.com>
2020-09-21 18:21:21 -0600

    Added directory for error files.

---
+++ Eeloo/Errors/Error.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+using Eeloo.Functions;
+using Eeloo.Grammar;
+using Eeloo.Objects;
+using Antlr4;
+using Antlr4.Runtime;
+
+namespace Eeloo.Errors
+{
+    class Error
+    {
+        public Error(ParserRuleContext error_context)
+        {
+            string err_msg = $"on line {error_context} ";
+
+        }
+    }
+}
