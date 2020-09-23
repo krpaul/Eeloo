4d6f29408249fb9292364766e0699a6ab1f88a79
krpaul <krpaul2004@gmail.com>
2020-09-23 12:07:01 -0600

    Added InvalidOperationError

---
+++ Eeloo/Errors/InvalidOperationError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime;
+
+namespace Eeloo.Errors
+{
+    class InvalidOperationError : BaseError
+    {
+        public InvalidOperationError(ParserRuleContext error_context, string operation, string type)
+            : base(error_context, "InvalidOperationError", $"Cannot perform {operation} operation on type {type}.")
+        { }
+    }
+}
