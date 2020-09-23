d6f33ec50184d0f1f15267fb8565dbbdc0694e64
krpaul <krpaul2004@gmail.com>
2020-09-23 12:06:11 -0600

    Added helper function to convert object types intro strings

---
+++ Eeloo/Helpers/ObjectTypeHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Eeloo.Objects;
+
+namespace Eeloo.Helpers
+{
+    class ObjectTypeHelpers
+    {
+        public string ObjectTypeToString(eeObjectType type)
+        {
+            switch (type)
+            {
+                case eeObjectType.BOOL:
+                    return "Boolean";
+                case eeObjectType.FUNCTION:
+                    return "Function";
+                case eeObjectType.LIST:
+                    return "List";
+                case eeObjectType.NUMBER:
+                    return "Number";
+                case eeObjectType.STRING:
+                    return "String";
+                default:
+                    return "<internal type>";
+            }
+        }
+    }
+}
