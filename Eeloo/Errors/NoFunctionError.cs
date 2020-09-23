dc3ac51bf0f652fcb7358c75cad56aa6191c6a01
krpaul <krpaul2004@gmail.com>
2020-09-23 12:07:31 -0600

    Added NoFunctionError

---
+++ Eeloo/Errors/NoFunctionError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+using Eeloo.Functions;
+using Eeloo.Grammar;
+using Eeloo.Objects;
+using Antlr4;
+using Antlr4.Runtime;
+
+namespace Eeloo.Errors
+{
+    class NoFunctionError : BaseError
+    {
+        public NoFunctionError(ParserRuleContext error_context, string fnName) 
+            : base(error_context, "NoFunctionError", $"Function with name {fnName} does not exist.")
+        {}
+    }
+}
