ce4f9801cf162120babf515bf81d184d77bd81d7
krpaul <34616839+krpaul@users.noreply.github.com>
2020-10-06 23:19:20 -0600

    Began implementing stack trace

---
+++ Eeloo/Objects/ParserObjects/StackTrace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime;
+using System.Collections.Generic;
+
+namespace Eeloo.Objects.ParserObjects
+{
+    class StackTrace
+    {
+        private List<ParserRuleContext> trace = new List<ParserRuleContext>();
+
+
+    }
+}
