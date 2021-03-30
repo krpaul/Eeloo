72f445dcd5cc14123f434f351c8bc321e508fa8b
Kevin Paul <krpaul2004@gmail.com>
2021-03-30 14:33:12 -0600

    Added folder for method related classes

---
+++ Eeloo/Methods/Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Eeloo.Objects;
+
+namespace Eeloo.Methods
+{
+    class Method
+    {
+        readonly string MethodID;
+        List<string> aliases;
+
+        eeObjectType MethodObjectType;
+
+        public Method(string id)
+        {
+        }
+    }
+}
