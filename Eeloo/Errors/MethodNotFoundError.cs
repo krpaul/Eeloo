2579fafe24e63b046a4bbc8de32e9ed14ae44f43
krpaul <34616839+krpaul@users.noreply.github.com>
2021-04-06 17:55:33 -0600

    Method not found error

---
+++ Eeloo/Errors/MethodNotFoundError.cs
@@ -0,0 +1,12 @@
+﻿using Eeloo.Objects;
+using Eeloo.Helpers;
+
+namespace Eeloo.Errors
+{
+    class MethodNotFoundError : BaseError
+    {
+        public MethodNotFoundError(string mName, eeObjectType mObjType)
+            : base("MethodNotFoundError", $"Method with name {mName} does not exist for object of type {ObjectTypeHelpers.ObjectTypeToString(mObjType)}.")
+        { }
+    }
+}
