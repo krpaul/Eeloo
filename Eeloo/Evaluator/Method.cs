aa356b24b16dec9cddf17cf8c77887722c088359
krpaul <34616839+krpaul@users.noreply.github.com>
2021-04-06 17:44:11 -0600

    Revert "Find and Call functions"
    This reverts commit 19106fc6e209f56380a497dc1f5f04acbead9db8.

--- Eeloo/Evaluator/Method.cs
+++ Eeloo/Evaluator/Method.cs
@@ -1,7 +1,6 @@
 ﻿using Antlr4.Runtime.Misc;
 using Eeloo.Grammar;
 using Eeloo.Objects;
-using Eeloo.Methods;
 using Eeloo.Errors;
 using System;
 
@@ -23,49 +22,21 @@ namespace Eeloo.Evaluator
 
         //public override eeObject VisitMethd
 
-        //public override eeObject VisitMethodCallExp([NotNull] EelooParser.MethodCallExpContext ctx)
-        //{
-        //    // add this to scope
-        //    Interpreter.currentScope.scopeCtx = ctx;
-
-        //    var hostObj = Visit(ctx.exp());
-        //    string methodName = ctx.fn_call().IDENTIFIER().GetText();
-
-        //    var passedParams = ctx.fn_call().exps();
-        //    var methodParams = passedParams != null ? Visit(passedParams) : null;
-
-        //    if (!hostObj.methods.ContainsKey(methodName))
-        //        throw new NoMethodError(methodName, hostObj.type);
-
-        //    return hostObj.CallMethod(methodName, methodParams);
-        //}
-
-        public override eeObject VisitMethod_standardSyntax([NotNull] EelooParser.Method_standardSyntaxContext ctx)
+        public override eeObject VisitMethodCallExp([NotNull] EelooParser.MethodCallExpContext ctx)
         {
             // add this to scope
             Interpreter.currentScope.scopeCtx = ctx;
 
-            eeObject obj = Visit(ctx.exp());
-            string alias = ctx.fn_call().IDENTIFIER().GetText();
+            var hostObj = Visit(ctx.exp());
+            string methodName = ctx.fn_call().IDENTIFIER().GetText();
 
             var passedParams = ctx.fn_call().exps();
             var methodParams = passedParams != null ? Visit(passedParams) : null;
 
-            Method m = Method.Find()
-        }
+            if (!hostObj.methods.ContainsKey(methodName))
+                throw new NoMethodError(methodName, hostObj.type);
 
-        public override eeObject VisitMethod_expandedSyntax([NotNull] EelooParser.Method_expandedSyntaxContext ctx)
-        {
-            // add this to scope
-            Interpreter.currentScope.scopeCtx = ctx;
-
-        }
-
-        public override eeObject VisitMethod_expandedSyntaxNoBrackets([NotNull] EelooParser.Method_expandedSyntaxNoBracketsContext ctx)
-        {
-            // add this to scope
-            Interpreter.currentScope.scopeCtx = ctx;
-
+            return hostObj.CallMethod(methodName, methodParams);
         }
     }
 }
