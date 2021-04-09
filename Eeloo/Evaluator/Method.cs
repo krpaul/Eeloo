0f5cb7ab231dba7f1b9540ac2faa35d82ad8f447
krpaul <34616839+krpaul@users.noreply.github.com>
2021-04-08 23:39:16 -0600

    Visitor functions for method syntaxes

--- Eeloo/Evaluator/Method.cs
+++ Eeloo/Evaluator/Method.cs
@@ -1,6 +1,7 @@
 ﻿using Antlr4.Runtime.Misc;
 using Eeloo.Grammar;
 using Eeloo.Objects;
+using Eeloo.Methods;
 using Eeloo.Errors;
 using System;
 
@@ -22,21 +23,91 @@ namespace Eeloo.Evaluator
 
         //public override eeObject VisitMethd
 
-        public override eeObject VisitMethodCallExp([NotNull] EelooParser.MethodCallExpContext ctx)
+        //public override eeObject VisitMethodCallExp([NotNull] EelooParser.MethodCallExpContext ctx)
+        //{
+        //    // add this to scope
+        //    Interpreter.currentScope.scopeCtx = ctx;
+
+        //    var hostObj = Visit(ctx.exp());
+        //    string methodName = ctx.fn_call().IDENTIFIER().GetText();
+
+        //    var passedParams = ctx.fn_call().exps();
+        //    var methodParams = passedParams != null ? Visit(passedParams) : null;
+
+        //    if (!hostObj.methods.ContainsKey(methodName))
+        //        throw new NoMethodError(methodName, hostObj.type);
+
+        //    return hostObj.CallMethod(methodName, methodParams);
+        //}
+
+        public override eeObject VisitMethod_standardSyntax([NotNull] EelooParser.Method_standardSyntaxContext ctx)
         {
             // add this to scope
             Interpreter.currentScope.scopeCtx = ctx;
 
-            var hostObj = Visit(ctx.exp());
-            string methodName = ctx.fn_call().IDENTIFIER().GetText();
+            eeObject obj = Visit(ctx.exp());
+            string alias = ctx.fn_call().IDENTIFIER().GetText();
 
             var passedParams = ctx.fn_call().exps();
             var methodParams = passedParams != null ? Visit(passedParams) : null;
 
-            if (!hostObj.methods.ContainsKey(methodName))
-                throw new NoMethodError(methodName, hostObj.type);
+            Method m = Method.Find(alias, obj.type, out Alias foundAlias);
 
-            return hostObj.CallMethod(methodName, methodParams);
+            // check if method wasn't found
+            if (m == null)
+                throw new MethodNotFoundError(alias, obj.type);
+
+            // check for flags
+            if (foundAlias.HasFlag(MethodFlag.NoStandardSyntax))
+                throw new Exception();
+
+            return m.Call(obj, methodParams.AsEXPRLIST());
+        }
+
+        public override eeObject VisitMethod_expandedSyntax([NotNull] EelooParser.Method_expandedSyntaxContext ctx)
+        {
+            // add this to scope
+            Interpreter.currentScope.scopeCtx = ctx;
+
+            eeObject obj = Visit(ctx.exp());
+            string alias = ctx.fn_call().IDENTIFIER().GetText();
+
+            var passedParams = ctx.fn_call().exps();
+            var methodParams = passedParams != null ? Visit(passedParams) : null;
+
+            Method m = Method.Find(alias, obj.type, out _);
+
+            if (m == null)
+                throw new MethodNotFoundError(alias, obj.type);
+
+            return m.Call(obj, methodParams.AsEXPRLIST());
+        }
+
+        public override eeObject VisitMethod_expandedSyntaxNoBrackets([NotNull] EelooParser.Method_expandedSyntaxNoBracketsContext ctx)
+        {
+            // add this to scope
+            Interpreter.currentScope.scopeCtx = ctx;
+
+            // add this to scope
+            Interpreter.currentScope.scopeCtx = ctx;
+
+            eeObject obj = Visit(ctx.exp());
+            string alias = ctx.IDENTIFIER().GetText();
+
+            Method m = Method.Find(alias, obj.type, out Alias foundAlias);
+
+            if (m == null)
+                throw new MethodNotFoundError(alias, obj.type);
+
+            // check if method wasn't found
+            if (m == null)
+                throw new MethodNotFoundError(alias, obj.type);
+
+            // check for flags
+            if (!foundAlias.HasFlag(MethodFlag.DontRequireBrackets))
+                throw new Exception();
+
+            return m.Call(obj, null);
         }
     }
 }
