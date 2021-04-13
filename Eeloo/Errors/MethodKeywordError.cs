f0fe4c156ed402f8984e85412d39a5ba88ca660c
krpaul <34616839+krpaul@users.noreply.github.com>
2021-04-12 22:36:01 -0600

    MethodKeywordError created

---
+++ Eeloo/Errors/MethodKeywordError.cs
@@ -0,0 +1,11 @@
+﻿namespace Eeloo.Errors
+{
+    class MethodKeywordError : BaseError
+    {
+        public MethodKeywordError(string mAlias, string keywordUsed, string[] validKeywords)
+            : base("MethodKeywordError", 
+                  $"Used wrong keyword \"{keywordUsed}\" for method {mAlias}, valid keywords are {string.Join(", ", validKeywords)}"
+            )
+        { }
+    }
+}
